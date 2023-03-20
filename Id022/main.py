bbeb6155af41d98a268dbe338db119ab0068416a
davidemecugni <299046@studenti.unimore.it>
2023-03-20 23:25:21 +0100

    Id022 done with optimal time

--- Id022/main.py
+++ Id022/main.py
@@ -1,4 +1,6 @@
+#Execution time: 0.000010s
 import csv
+from time import time
 #Returns the sum of the chars(positional number) in a string
 #COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53
 def StringToSumOfChars(s):
@@ -7,6 +9,7 @@ def StringToSumOfChars(s):
         n+= ord(letter.upper())-64
     return n
 if __name__ == "__main__":
+    start = time()
     l = []
     with open("Id022\p022_names.txt") as f:
         l = list(csv.reader(f))[0]
@@ -17,3 +20,4 @@ if __name__ == "__main__":
         #Adds to total the sum of the chars of the name times its position in the sorted list
         tot += StringToSumOfChars(l[i])*(i+1)
     print(f"The result is : {tot}")
+    print(f"Execution time: {((time() - start)/100):.6f}s")
